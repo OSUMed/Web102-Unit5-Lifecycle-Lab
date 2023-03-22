ac415d9998bdf04e372adc1ea70641f0a4317e6f
Sri <srikanthsmedicherla@gmil.com>
2023-03-21 18:24:46 -0700

    feat: checkpoint 1

--- web102-lab5/src/App.jsx
+++ web102-lab5/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+const API_KEY = import.meta.env.MAIN_API_KEY;
+const MAIN_API_KEY =
+  "ab8436e818f95b539f14019303dedcb76ec04cf7fd6b131ca4e516a2d4f45202";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+    const fetchAllCoinData = async () => {
+      const response = await fetch(
+        "https://min-api.cryptocompare.com/data/all/coinlist?&api_key" +
+          MAIN_API_KEY
+      );
+      const json = await response.json();
+      setList(json);
+    };
+    fetchAllCoinData().catch(console.error);
+    console.log("can u print this: ", list);
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="whole-page">
+      <h1>My Crypto List</h1>
+
+      <ul>
+        {" "}
+        {list &&
+          Object.entries(list.Data).map(([coin]) =>
+            list.Data[coin].PlatformType === "blockchain" ? (
+              <li key={list.Data[coin].FullName}>{list.Data[coin].FullName}</li>
+            ) : null
+          )}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
